f96dced5bab3fd5eb6539011dc45dbb3afabf6e0
Mauricio Ballesteros <mballes95@gmail.com>
2020-05-11 15:27:45 -0400

    Add Login and User

--- src/app.py
+++ src/app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+import functools
+import os
 
-from .models import database, Video
+from flask import Flask, render_template, request, g, session, redirect, url_for, flash
+from playhouse.shortcuts import model_to_dict
+from werkzeug.security import check_password_hash
+
+from .models import database, User, Video
 
 app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY', 'my-secret-key')
 
-# Middlewares
+
+"""
+Middlewares
+"""
 @app.before_request
 def before_request():
     database.connect()
@@ -14,7 +23,54 @@ def after_request(response):
     database.close()
     return response
 
-# Request
-@app.route('/')
+def login_required(f):
+    @functools.wraps(f)
+    def inner(*args, **kwargs):
+        if not session.get('user'):
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return inner
+
+
+"""
+Auth
+"""
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if session.get('user'):
+        return redirect(url_for('index'))
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.select().where(User.username == username).first()
+        error = None
+
+        if user is None:
+            error = "Incorrect username."
+        elif not check_password_hash(user.password, password):
+            error = "Incorrect password."
+            
+        if error is None:
+            session.clear()
+            session['user'] = model_to_dict(user)
+            return redirect(url_for('index'))
+
+        flash(error)
+        
+    return render_template('login.html')
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+
+"""
+Routes
+"""
+@app.route('/', methods=['GET'])
+@login_required
 def index():
     return render_template('index.html')
